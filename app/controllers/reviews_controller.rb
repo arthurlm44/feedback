ac0401c18df67795826152159d130259de7e907c
Adam Ellsworth, Arthur Le Maitre and Paulette Luftig <pair+azellsworth+arthur+paulette@mavenlink.com>
2016-03-18 14:07:50 -0700

    make new review form work with statement scoring

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,27 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new
+    @reviewer = current_user
+    @users = User.all.reject { |u| u.id == @reviewer.id }
+    @statements = Statement.all
+    @statements.each { |statement| @review.statement_scores.build(statement_id: statement.id) }
   end
 
   def create
+    review = Review.new(review_params)
+    if review.save
+      flash[:notice] = 'Great success! Niiiice, I like'
+      redirect_to root_path
+    else
+      flash[:notice] = 'You dun messed up'
+    end
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :user_id, :reviewer_id, statement_scores_attributes: [:statement_id, :score])
+  end
+
 end
--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,3 +1,4 @@
 class Statement < ActiveRecord::Base
+  has_many :statement_scores
   has_many :reviews, through: :statement_scores
 end
