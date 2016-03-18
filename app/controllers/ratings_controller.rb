e10f99b97ba6d53e47d1d06b7a3702932bd541bd
Adam Ellsworth, Arthur Le Maitre and Paulette Luftig <pair+azellsworth+arthur+paulette@mavenlink.com>
2016-03-18 15:46:03 -0700

    rate feedback

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,8 +4,25 @@ class RatingsController < ApplicationController
   end
 
   def new
+    @rating = Rating.new
+    blacklist = Rating.where(user_id: 4).pluck(:review_id)
+    @next_up = Review.where("id NOT IN (?)", blacklist).where('rating_count < 3').order('created_at ASC').first
+    # @next_up = Review.where('rating_count <= ?', 3).order('created_at ASC').first
   end
 
   def create
+    rating = Rating.new(rating_params)
+    if rating.save
+      flash[:notice] = 'Great success! Niiiice, I like'
+      redirect_to new_rating_path
+    else
+      flash[:notice] = 'You dun messed up'
+    end
+  end
+
+
+  private
+  def rating_params
+    params.require(:rating).permit(:review_id, :user_id, :specificity, :actionability, :kindness)
   end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,12 @@
 class Rating < ActiveRecord::Base
   belongs_to :review
   belongs_to :user
+
+  after_save :update_rating_count
+
+  def update_rating_count
+    review = Review.find(self.review_id)
+    review.rating_count += 1
+    review.save
+  end
 end
